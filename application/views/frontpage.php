f4f751463666161d97d7bdb2fdc0d7cb922aa332
unknown <ramir92m@gmail.com>
2015-04-02 17:25:36 -0800

    Initial Development

---
+++ application/views/frontpage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>NoteApp - Simple, ease of use note taking application</title>
+        <meta charset="UTF-8">
+        <meta name="Author" content="Ramir Manarpaac">
+        <meta name="Description" content="A simple, ease of use note taking app">
+    </head>
+    <body>
+        
+        Sample 123 Sample
+        
+        <!-- Header Section -->
+<!--        <div id="header">
+            <a href="#">Logo Here</a>
+        </div>-->
+        <!--End Header -->
+        
+    </body>
+</html>

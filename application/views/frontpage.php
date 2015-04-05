2d0f7842c38af397eeb50fa40488f93559935619
ramir92m <ramir92m@gmail.com>
2015-04-05 19:18:00 +0800

    Working on header section and content (unfinish)

--- application/views/frontpage.php
+++ application/views/frontpage.php
@@ -3,18 +3,12 @@
     <head>
         <title>NoteApp - Simple, ease of use note taking application</title>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="Author" content="Ramir Manarpaac">
         <meta name="Description" content="A simple, ease of use note taking app">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url()."pure/css/pure.css"; ?>" />
     </head>
     <body>
-        
-        Sample 123 Sample
-        
-        <!-- Header Section -->
-<!--        <div id="header">
-            <a href="#">Logo Here</a>
-        </div>-->
-        <!--End Header -->
         
     </body>
 </html>

3e135d8eab1f7e6e2e705706629aba597e36d0e8
ramir92m <ramir92m@gmail.com>
2015-04-05 19:21:58 +0800

    Mock up for frontpage

---
+++ application/views/mockup_frontpage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>NoteApp - Simple, ease of use note taking application</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="Author" content="Ramir Manarpaac">
+        <meta name="Description" content="A simple, ease of use note taking app">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url()."css/main/font.css" ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url()."css/pure/pure.css"; ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url()."css/main/front.css"; ?>" />
+    </head>
+    <body>
+        <!-- Header -->
+        <div id="header">
+            <div class="container">
+                <div class="pure-menu pure-menu-horizontal" >
+                    <a href="#" class="pure-menu-heading pure-menu-link brand"><b>NoteApp</b></a>
+                    <ul class="pure-menu-list right">
+                        <li class="pure-menu-item"><a href="#" class="pure-button pure-button-primary">Sign-Up</a></li>
+                        <li class="pure-menu-item"><a href="#" class="pure-menu-link login">Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- End Header -- >
+        
+        <!-- Content -->
+        <div class="container">
+            <div id="content">
+                <h1>Clean, Simplify Notes</h1>
+            </div>
+        </div>
+        <!-- End Content -->
+    </body>
+</html>
